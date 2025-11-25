740d6a6b72160f76cb580e464cc016dfdc8d2122
Hetmanchyk Ruslan <ruslan.hetmanchyk@gmail.com>
2025-11-25 11:34:09 +0300

    Refactoring

--- Assets/Scripts/Gameplay/HexItemView.cs
+++ Assets/Scripts/Gameplay/HexItemView.cs
@@ -1,45 +1,29 @@
 ﻿using Core.Services.Gameplay.Level.Implementation;
 using Helpers;
 using UnityEngine;
-using Zenject;
 
 namespace Gameplay
 {
-    public class HexItemView : MonoBehaviour, IPoolable<HexItem, IMemoryPool>
+    public class HexItemView : MonoBehaviour
     {
         [SerializeField] private HexItemAnimator animator;
         [SerializeField] private Renderer rend;
         [SerializeField] private float height = 0.35f;
 
-        private IMemoryPool pool;
-
         public HexItemAnimator Animator => animator;
         public float Height => height;
-
-        public void ApplyColorById(int idx)
-        {
-            if (rend.sharedMaterial != null)
-            {
-                rend.material.color = ColorHelper.Palette[idx];
-            }
-        }
 
         public void Init(HexItem model)
         {
             ApplyColorById(model.ColorId);
         }
 
-        public void OnDespawned()
+        private void ApplyColorById(int idx)
         {
-            gameObject.SetActive(false);
-            pool = null;
-        }
-
-        public void OnSpawned(HexItem model, IMemoryPool pool)
-        {
-            this.pool = pool;
-            Init(model);
-            gameObject.SetActive(true);
+            if (rend.sharedMaterial != null)
+            {
+                rend.material.color = ColorHelper.Palette[idx];
+            }
         }
     }
 }
